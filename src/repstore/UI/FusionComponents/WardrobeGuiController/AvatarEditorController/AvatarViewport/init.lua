dd82c80e6e326f3bc40ad368606fe6fd580c8af0
Celina Sherlock <16767-1@UKARSWD59797.global.umusic.net>
2025-11-17 18:09:22 +0000

    add new 'my outfits' button to avatar viewport

--- src/repstore/UI/FusionComponents/WardrobeGuiController/AvatarEditorController/AvatarViewport/init.lua
+++ src/repstore/UI/FusionComponents/WardrobeGuiController/AvatarEditorController/AvatarViewport/init.lua
@@ -19,6 +19,8 @@ local OutfitClientService = require(Utility:WaitForChild("OutfitClientService"))
 -- Fusion
 local peek = Fusion.peek
 local Children = Fusion.Children
+type UsedAs<T> = Fusion.UsedAs<T>
+
 
 -- GUI Components
 local RotateButton = require(script:WaitForChild("RotateButton"))
@@ -35,7 +37,22 @@ local CONFIG = {
 
 function AvatarViewport(
 	scope: Fusion.Scope,
-	model: Fusion.UsedAs<Model> -- Changed from Value<Model> to UsedAs<Model>
+	model: Fusion.UsedAs<Model>, -- Changed from Value<Model> to UsedAs<Model>,
+	props: {
+		size: UsedAs<UDim2>?,
+		position: UsedAs<UDim2>?,
+		anchorPoint: UsedAs<Vector2>?,
+		layoutOrder: UsedAs<number>?,
+		backgroundColor3: UsedAs<Color3>?,
+		backgroundTransparency: UsedAs<number>?,
+		visible: UsedAs<boolean>?,
+		name: UsedAs<string>?,
+		isSelected: UsedAs<boolean>?,
+		currentView: Fusion.Value<string>,
+		searchAssetCategories: Fusion.Value<{Enum.AvatarAssetType}>,
+		searchBundleCategories: Fusion.Value<{Enum.BundleType}>,
+		searchCallback: () -> ()
+	}
 ): ViewportFrame
 	-- Avatar manipulation variables
 	local pitch = scope:Value(0)
@@ -122,6 +139,19 @@ function AvatarViewport(
 				
 				onActivated = function()
 					OutfitClientService.ResetPlayerOutfit(localPlayer)
+				end,
+			}),
+
+			Button(scope, {
+				name = "OutfitsButtonFrame",
+				text = "My Outfits",
+				size = UDim2.fromScale(0.3, 0.1), 
+				position = UDim2.fromScale(0,0),
+				anchorPoint = Vector2.new(0,0),
+				zIndex = 2,
+
+				onActivated = function()
+					props.currentView:set("Outfits")
 				end,
 			}),
 
