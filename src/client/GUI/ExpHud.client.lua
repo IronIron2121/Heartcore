708d7d8abf112fd67e850a1381757ac829ba0ede
Okafor <gabriel.okafor@umusic.com>
2025-11-18 11:35:38 +0000

    fix bug on rank names

--- src/client/GUI/ExpHud.client.lua
+++ src/client/GUI/ExpHud.client.lua
@@ -30,12 +30,12 @@ local ExpBar = require(FusionComponents:WaitForChild("ExpBar"))
 
 local leaderstats = localPlayer:WaitForChild("leaderstats")
 local level = leaderstats:WaitForChild("Level")
-local levelName = leaderstats:WaitForChild("LevelName")
+local Rank = leaderstats:WaitForChild("Rank")
 
-local rankText = Fusion.Value(scope, levelName.Value)
+local rankText = Fusion.Value(scope, Rank.Value)
 
 --Anim function
-local function animateLevelName(label)
+local function animateRank(label)
 	
 	-- Store original properties
 	local originalSize = label.Size
@@ -157,10 +157,10 @@ local function initialiseGUI()
 		}
 	}
 	
-	levelName:GetPropertyChangedSignal("Value"):Connect(function()
-		rankText:set(levelName.Value .. " (Lv. " .. level.Value .. ")")
+	Rank:GetPropertyChangedSignal("Value"):Connect(function()
+		rankText:set(Rank.Value .. " (Lv. " .. level.Value .. ")")
 
-		task.spawn(animateLevelName, playerRankLabel)
+		task.spawn(animateRank, playerRankLabel)
 	end)
 	
 end
--- src/repstore/UI/FusionComponents/ExpBar.lua
+++ src/repstore/UI/FusionComponents/ExpBar.lua
@@ -9,16 +9,21 @@ local Players = game:GetService("Players")
 local Utility = ReplicatedStorage:WaitForChild("Utility")
 local DataTables = ReplicatedStorage:WaitForChild("DataTables")
 
-
 -- Modules
 local peek = require(ReplicatedStorage.Utility.Fusion.State.peek)
 local Fusion = require(Utility:WaitForChild("Fusion"))
 local ImageUris = require(DataTables:WaitForChild("ImageUris"))
 
-
 -- Fusion
 local Children = Fusion.Children
 type UsedAs<T> = Fusion.UsedAs<T>
+
+
+-- Instances
+local localPlayer = Players.LocalPlayer
+
+local leaderstats = localPlayer:WaitForChild("leaderstats")
+local exp = leaderstats:WaitForChild("Exp")
 
 function ExpBar(
     scope: Fusion.Scope,
@@ -40,7 +45,10 @@ function ExpBar(
 		zIndex: UsedAs<number>?,
 		onActivated: (() -> ())?,
 	}
-): Frame 
+): Frame
+
+    local scale = (exp.Value % 10) * 0.075
+    local expBarSize = Fusion.Value(scope, UDim2.fromScale(scale, 0.15))
 
     -- Tween settings
     local tweenInfo = TweenInfo.new(
@@ -74,6 +82,7 @@ function ExpBar(
         tween:Play()
     end 
 
+    exp:GetPropertyChangedSignal("Value"):Connect(updateExpBar)
 
     local frame = scope:New "Frame" {
         Name = "ExpBarContainer",
@@ -97,32 +106,36 @@ function ExpBar(
                 [Children] = {
                     scope:New "UIAspectRatioConstraint" {
                         AspectRatio = 3,
+                    },
+                    
+                    scope:New "Frame" {
+                        Name = "ProgressFill",
+                        AnchorPoint = Vector2.new(0,0.5),
+                        Size = expBarSize,
+                        Position = UDim2.fromScale(0.2,0.47),
+                        BackgroundColor3 = Color3.new(1,1,1),
+                        ZIndex = 1,
+
+                        [Children] = {
+                            scope:New "UIGradient"{
+                                Color = ColorSequence.new(Color3.fromRGB(24, 107, 79), Color3.fromRGB(130, 194, 144)),
+                            },
+
+                            scope:New "UICorner" {
+                                CornerRadius = UDim.new(0.5,0)
+                            },
+
+                            --[[
+                            scope:New "UIAspectRatioConstraint" {
+                                AspectRatio = 10,
+                            }
+                            ]]
+                        }
                     }
                 }
             },
             
-            scope:New "Frame" {
-                Name = "ProgressFill",
-                AnchorPoint = Vector2.new(0,0.5),
-                Size = UDim2.fromScale(0.75,0.15),
-                Position = UDim2.fromScale(0.2,0.47),
-                BackgroundColor3 = Color3.new(1,1,1),
-                ZIndex = 1,
 
-                [Children] = {
-                    scope:New "UIGradient"{
-                        Color = ColorSequence.new(Color3.fromRGB(24, 107, 79), Color3.fromRGB(130, 194, 144)),
-                    },
-
-                    scope:New "UICorner" {
-                        CornerRadius = UDim.new(0.5,0)
-                    },
-
-                    scope:New "UIAspectRatioConstraint" {
-                        AspectRatio = 10,
-                    }
-                }
-            }
         }
     } :: Frame
 
