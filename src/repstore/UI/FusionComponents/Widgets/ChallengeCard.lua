0e8ee0a308eb145817f6b9f2c19ab8d5194e07bd
Okafor <gabriel.okafor@umusic.com>
2025-11-18 17:05:45 +0000

    wip

--- src/repstore/UI/FusionComponents/Widgets/ChallengeCard.lua
+++ src/repstore/UI/FusionComponents/Widgets/ChallengeCard.lua
@@ -225,7 +225,7 @@ local function ChallengeCard(
                             scope:New "UIStroke" {
                                 ApplyStrokeMode = Enum.ApplyStrokeMode.Border,
                                 Thickness = 3,
-                                Color = props.isClaimed and Color3.fromRGB(0,100,10) and  Color3.fromRGB(200, 120, 0)
+                                Color = props.isClaimed and Color3.fromRGB(0,100,10) or Color3.fromRGB(200, 120, 0)
                             }
                         }
                     }
