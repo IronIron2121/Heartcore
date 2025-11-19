73a143faddbd2a45f15383f51d305f9628d4ccec
IronIron2121 <gabeoak@outlook.com>
2025-11-19 10:49:40 +0000

    remove some challenges

--- src/server/DailyChallenges/ChallengeDefinitions.lua
+++ src/server/DailyChallenges/ChallengeDefinitions.lua
@@ -46,56 +46,7 @@ ChallengeDefinitions.ALL_CHALLENGES = {
         }
     },
 
-    VOTE_10_OUTFITS = {
-        id = "VOTE_10_OUTFITS",
-        type = "VOTE_OUTFIT",
-        name = "Style Critic",
-        description = "Vote on 10 different outfits",
-        targetAmount = 10,
-        reward = {
-            exp = 25,
-            currency = 150
-        }
-    },
-
-    -- Viewing challenges
-    VIEW_20_OUTFITS = {
-        id = "VIEW_20_OUTFITS",
-        type = "VIEW_OUTFITS",
-        name = "Window Shopper",
-        description = "View 20 different outfits",
-        targetAmount = 20,
-        reward = {
-            exp = 10,
-            currency = 50
-        }
-    },
-
-    -- Competition challenges
-    GET_5_VOTES = {
-        id = "GET_5_VOTES",
-        type = "WIN_VOTES",
-        name = "Rising Star",
-        description = "Receive 5 votes on your outfit",
-        targetAmount = 5,
-        reward = {
-            exp = 20,
-            currency = 100
-        }
-    },
-
-    GET_20_VOTES = {
-        id = "GET_20_VOTES",
-        type = "WIN_VOTES",
-        name = "Fashion Icon",
-        description = "Receive 20 votes on your outfit",
-        targetAmount = 20,
-        reward = {
-            exp = 50,
-            currency = 250
-        }
-    },
-
+    --[[
     -- Login streak (auto-awarded)
     LOGIN_STREAK_3 = {
         id = "LOGIN_STREAK_3",
@@ -108,18 +59,9 @@ ChallengeDefinitions.ALL_CHALLENGES = {
             currency = 150
         }
     },
+    ]]
 
-    LOGIN_STREAK_7 = {
-        id = "LOGIN_STREAK_7",
-        type = "LOGIN_STREAK",
-        name = "Fashion Devotee",
-        description = "Log in for 7 days in a row",
-        targetAmount = 7,
-        reward = {
-            exp = 100,
-            currency = 500
-        }
-    },
+
 }
 
 -- Get all daily challenges
