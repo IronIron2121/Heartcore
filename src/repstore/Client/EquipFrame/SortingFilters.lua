f5945429aeae21b717395692a554e7558f15e01f
Celina Sherlock <16767-1@UKARSWD59797.global.umusic.net>
2025-11-20 17:51:34 +0000

    sort srop down tweaks

--- src/repstore/Client/EquipFrame/SortingFilters.lua
+++ src/repstore/Client/EquipFrame/SortingFilters.lua
@@ -7,21 +7,22 @@
 
 local SortingFilters = {
 	list = {
-		"Bestselling",
-		"Most favorited",
-		"Recently created",
-		"Relevance",
-		"Price ascending",
-		"Price descending",
+		"Sort by: Bestselling",
+		"Sort by: Most favorited",
+		"Sort by: Recently created",
+		"Sort by: Relevance",
+		"Sort by: Price ascending",
+		"Sort by: Price descending",
 	},
 	filters = {
-		["Bestselling"] = Enum.CatalogSortType.Bestselling,
-		["Most favorited"] = Enum.CatalogSortType.MostFavorited,
-		["Recently created"] = Enum.CatalogSortType.RecentlyCreated,
-		["Relevance"] = Enum.CatalogSortType.Relevance,
-		["Price ascending"] = Enum.CatalogSortType.PriceLowToHigh,
-		["Price descending"] = Enum.CatalogSortType.PriceHighToLow,
+		["Sort by: Bestselling"] = Enum.CatalogSortType.Bestselling,
+		["Sort by: Most favorited"] = Enum.CatalogSortType.MostFavorited,
+		["Sort by: Recently created"] = Enum.CatalogSortType.RecentlyCreated,
+		["Sort by: Relevance"] = Enum.CatalogSortType.Relevance,
+		["Sort by: Price ascending"] = Enum.CatalogSortType.PriceLowToHigh,
+		["Sort by: Price descending"] = Enum.CatalogSortType.PriceHighToLow,
 	},
 }
 
 return SortingFilters
+ 
