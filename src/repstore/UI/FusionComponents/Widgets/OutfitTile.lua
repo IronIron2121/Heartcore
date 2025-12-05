16f084d873a8b9f7887b68234f9688ca9acf671e
Celina Sherlock <16767-1@UKARSWD59797.global.umusic.net>
2025-12-05 12:57:35 +0000

    replace delete button in my outfits tab

--- src/repstore/UI/FusionComponents/Widgets/OutfitTile.lua
+++ src/repstore/UI/FusionComponents/Widgets/OutfitTile.lua
@@ -24,6 +24,11 @@ local Children = Fusion.Children
 type UsedAs<T> = Fusion.UsedAs<T>
 local peek = Fusion.peek
 
+-- Constants
+local COLOUR_ORANGE 			= Color3.new(0.901961, 0.380392, 0.078431)
+local COLOUR_GREY 				= Color3.new(1, 1, 1)
+local BG_FADE_SPEED 			= 20
+
 --
 
 function OutfitTile(
@@ -39,6 +44,7 @@ function OutfitTile(
 		outfit: Fusion.UsedAs<{}>?,
 		onSelect: () -> (),
 		onDelete: () -> ()?,
+		onClick: () -> (),
 	}
 ): Frame
 	warn("making outfit tile for, ", props.humanoidDescription)
@@ -67,6 +73,18 @@ function OutfitTile(
 		end
 	end)
 
+		-- State management
+	local Toggled = scope:Value(false)
+	local isHovering = scope:Value(false)
+	local isHeldDown = scope:Value(false)
+
+	local COLOUR_BG_TOGGLED = COLOUR_ORANGE
+	local COLOUR_BG_NOT_TOGGLED = COLOUR_GREY
+
+	local isClicked = scope:Computed(function(use, _)
+		return use(Toggled) == true
+	end)
+
 	-- Create viewport camera
 	local viewportCamera = scope:Value(nil)
 
@@ -88,19 +106,51 @@ function OutfitTile(
 			},
 
 			scope:New "ImageButton" {
-				Name = "CloseButton",
-				Image = ImageUris["CloseButton"],
+				Name = "RemoveButton",
 				AnchorPoint = Vector2.new(0.5, 0.5),
+				Position = UDim2.fromScale(0.9, 0.01),
 				Size = UDim2.fromScale(0.2, 0.2),
+				ZIndex = 3,
 				BackgroundTransparency = 1,
-				Position = UDim2.fromScale(0.9,0),
-				ZIndex = 2,
+				Image = ImageUris.TrashButton,
+				Active = true,
+
+				[OnEvent "Activated"] = function()
+					if props.onClick ~= nil then
+						props.onClick()
+					end
+				end,
 				
-				[Children] = {
-					scope:New "UIAspectRatioConstraint" {
-						AspectRatio = 1
-					}
-				},
+				[OnEvent "MouseButton1Down"] = function()
+					isHeldDown:set(true)
+				end,
+				
+				[OnEvent "MouseButton1Up"] = function()
+					isHeldDown:set(false)
+				end,
+				
+				[OnEvent "MouseEnter"] = function()
+					isHovering:set(true)
+				end,
+				
+				[OnEvent "MouseLeave"] = function()
+					isHovering:set(false)
+				end,
+
+				ImageColor3 = scope:Spring(
+					scope:Computed(function(use)
+						local baseColor = use(isClicked) and COLOUR_BG_TOGGLED or COLOUR_BG_NOT_TOGGLED
+						
+						if use(isHeldDown) then
+							return baseColor:Lerp(COLOUR_ORANGE, 0.8)
+						elseif use(isHovering) then
+							return baseColor:Lerp(COLOUR_ORANGE, 0.25)
+						else 
+							return baseColor
+						end
+					end),
+					BG_FADE_SPEED
+				), 
 				
 				[OnEvent "Activated"] = function()
 					if props.onDelete then
