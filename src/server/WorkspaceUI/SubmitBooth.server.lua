0e6be1c5eb8e2eeaa371fa46db871befe02b8643
Okafor <gabriel.okafor@umusic.com>
2026-02-04 11:20:28 +0000

    Add timer to submission hut

--- src/server/WorkspaceUI/SubmitBooth.server.lua
+++ src/server/WorkspaceUI/SubmitBooth.server.lua
@@ -1,8 +1,16 @@
 --!strict
 
+-- Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
 -- Folders
 local submissionZone = workspace:WaitForChild("submissionZone")
 local centralPond = workspace:WaitForChild("centralPond")
+local Values = ReplicatedStorage:WaitForChild("Values")
+
+-- Values
+local currentStateName = Values:WaitForChild("CurrentStateName")
+local secondsRemaining = Values:WaitForChild("SecondsRemaining")
 
 -- Instances
 local submissionHut = submissionZone.submissionHut
@@ -10,16 +18,10 @@ local guiFrame = submissionHut.guiFrame
 local surfaceGui = guiFrame.SurfaceGui
 local timerLabel = surfaceGui.TimerLabel :: TextLabel
 
-
-local centralPondModel = centralPond.centralPond
-local SubmissionBillboardHolder = centralPondModel.SubmissionBillboardHolder
-local BillboardGui = SubmissionBillboardHolder.BillboardGui
-local Frame = BillboardGui.Frame
-local TimeLabel = Frame.TimeLabel :: TextLabel
-
-task.spawn(function()
-    while true do
-        task.wait(1)
-        timerLabel.Text = TimeLabel.Text
+secondsRemaining:GetPropertyChangedSignal("Value"):Connect(function(...)  
+    if currentStateName.Value == "Dressing" then
+        timerLabel.Text = secondsRemaining.Value
+    else
+        timerLabel.Text = "Waiting..."
     end
 end)
