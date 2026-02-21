a2960aabc209a955e175be863f2612a61a4b5da3
Okafor <gabriel.okafor@umusic.com>
2026-02-21 23:59:46 +0000

    Fix OOF sound

--- src/repstore/Utility/teleportPlayer.lua
+++ src/repstore/Utility/teleportPlayer.lua
@@ -8,10 +8,13 @@
 -- Local Constants
 local TELEPORT_Y_OFFSET = 10
 
-function teleportPlayer(player: Player, destination: BasePart)
+function teleportPlayer(player: Player, destination: BasePart, offSet: Vector3?)
 	local playerCharacter = player.Character or player.CharacterAdded:Wait()
 	local teleportOffset = Vector3.new(0, TELEPORT_Y_OFFSET, 0)
-	
+	if offSet then
+		teleportOffset += offSet
+	end
+
 	playerCharacter:PivotTo(destination:GetPivot() + teleportOffset)
 end
 
